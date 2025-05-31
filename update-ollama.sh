45ed07431291cdce1d56d32ef6a4ae3744a04d02
ppogreba <paulpogreba@gmail.com>
2025-05-31 04:53:21 +0000

    fx

--- update-ollama.sh
+++ update-ollama.sh
@@ -28,7 +28,6 @@ if [ "${openwebui,}" == "y" ]; then
     echo "starting Open-WebUI"
     docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:latest
     echo "You can now access open-webui via http://127.0.0.1:11434, http://{host_ip}:11434, or preconfigured proxy redirect"
-    exit 1
 else
     echo "No changes to Openwebui made"
 fi 
